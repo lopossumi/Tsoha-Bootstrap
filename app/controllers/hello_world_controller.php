7164e61b1a51f98980d9beb5a2f90bf1d497a742
Mikko Loponen <mikko.loponen@iki.fi>
2016-09-18 20:44:48 +0300

    new task dialog

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -3,17 +3,18 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
    	  View::make('login.html');
     }
 
     public static function home(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
    	  View::make('home.html');
     }
 
+    public static function newtask(){
+   	  View::make('newtask.html');
+    }
+
     public static function sandbox(){
-      // Testaa koodiasi täällä
       echo 'Hello World!';
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,10 @@
   $routes->get('/', function() {
     HelloWorldController::index();
   });
-
   $routes->get('/home', function() {
     HelloWorldController::home();
   });
+  $routes->get('/newtask', function() {
+    HelloWorldController::newtask();
+  });
+
