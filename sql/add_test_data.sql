02c4960383294fb90c84ebe4caed42dd6ba55d2a
Mikko Loponen <mikko.loponen@iki.fi>
2016-09-19 23:47:46 +0300

    sql-lauseet erittäin ajoissa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Human (username, fullname, password, email) VALUES('spede1', 'Pertti Pasanen', 'spede123', 'spede@spe.de');
+INSERT INTO TaskList (listname, description) VALUES('Koti', 'Ruoka, sisustus ja muu paskartelu');
+INSERT INTO Task (description, duedate, priority, status) VALUES('Tee se', NOW(), 3, 0);
+INSERT INTO Category (description, symbol) VALUES ('MyCategory1', 33);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,37 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Human(
+    id SERIAL PRIMARY KEY,
+    username varchar(20) NOT NULL, 
+    fullname varchar(100) NOT NULL,
+    password varchar(50) NOT NULL,
+    email varchar(100) NOT NULL,
+    isprivate boolean DEFAULT TRUE,
+    isadmin boolean DEFAULT FALSE
+);
+
+CREATE TABLE TaskList(
+    id SERIAL PRIMARY KEY,
+    id_owner INTEGER REFERENCES Human(id),
+    listname varchar(50) NOT NULL,
+    description varchar(100)
+);
+
+CREATE TABLE Task(
+    id SERIAL PRIMARY KEY,
+    id_tasklist INTEGER REFERENCES TaskList(id),
+    description varchar(100) NOT NULL,
+    duedate timestamp,
+    priority INTEGER,
+    status INTEGER
+);
+
+CREATE TABLE Category(
+    id SERIAL PRIMARY KEY,
+    id_owner INTEGER REFERENCES Human(id),
+    description varchar(50) NOT NULL,
+    symbol INTEGER
+);
+
+CREATE TABLE TaskCategory(
+    id_task INTEGER REFERENCES Task(id),
+    id_category INTEGER REFERENCES Category(id)
+);
