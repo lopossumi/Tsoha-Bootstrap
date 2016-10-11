08c376e6e3369427a160d2822d7785986c1b0481
Mikko Loponen <mikko.loponen@iki.fi>
2016-10-11 15:51:38 +0300

    missing $

--- app/models/category.php
+++ app/models/category.php
@@ -77,7 +77,7 @@ class Category extends BaseModel{
     		DELETE FROM taskcategory 
     		      WHERE id_task = :id_task');
         $query->bindValue(':id_task', $id_task, PDO::PARAM_INT);
-        query->execute();
+        $query->execute();
     }
 
     public static function insert($id_task, $id_category){
