4576b2b3f871de13b29ee4855c8f13318d20dd58
Mikko Loponen <mikko.loponen@iki.fi>
2016-10-16 17:40:39 +0300

    test data

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,17 +4,17 @@ VALUES('spede1', 'Pertti Pasanen', '$1$IVbaNVZd$QW60i6BtJ7N5DJI1ZUZOD1', 'spede@
 INSERT INTO TaskList (id_owner, name, description) VALUES ('1', 'Koti', 'Ruoka, sisustus ja muu paskartelu');
 INSERT INTO TaskList (id_owner, name, description) VALUES ('1', 'Lomamatka', 'Lomareissu Teneriffalle');
 
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Imuroi',          '2016-10-17 16:00:00',  3, 0);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Tiskaa astiat',   '2016-10-16 16:00:00',  2, 0);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Vie roskat',      '2016-10-15 16:00:00',  1, 0);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Huilaa hetkinen', NULL,                   4, 1);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Keksi keksintö',  '2016-10-14 16:00:00',  3, 2);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Imuroi astiat',           '2016-10-17 16:00:00',  3, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Tiskaa lattiat',          '2016-10-16 16:00:00',  2, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Kastele stereot',         '2016-10-15 16:00:00',  1, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Huilaa hetkinen',         NULL,                   4, 1);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('1', 'Keksi keksintö',          '2016-10-14 16:00:00',  3, 2);
 
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa lennot',        '2016-10-26 16:00:00',  3, 2);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa hotelli',       '2016-10-25 16:00:00',  2, 1);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa vuokra-auto',   '2016-10-24 16:00:00',  1, 0);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Lataa offline-kartta',NULL,                   4, 0);
-INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Osta matkalaukku',    '2016-10-23 16:00:00',  3, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa lennot',            '2016-10-26 16:00:00',  3, 2);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa hotelli',           '2016-10-25 16:00:00',  2, 1);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Varaa vuokra-auto',       '2016-10-24 16:00:00',  1, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Lataa offline-kartta',    NULL,                   4, 0);
+INSERT INTO Task (id_tasklist, name, duedate, priority, status)  VALUES('2', 'Osta matkalaukku',        '2016-10-23 16:00:00',  3, 0);
 
 INSERT INTO Category (id_owner, name, symbol, color) VALUES ('1', 'Ostokset',       'shopping-cart',    'primary');
 INSERT INTO Category (id_owner, name, symbol, color) VALUES ('1', 'Koti',           'home',             'info');
